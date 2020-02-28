50866de215b34af8b242834cdb2394b3ae43f5eb
Anh Vuong <vuonga1103@yahoo.com>
2020-02-28 15:39:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var shout = str =>  str.toUpperCase();
+
+var whisper = str => str.toLowerCase();
+
+var logShout = str => console.log(str.toUpperCase());
+
+var logWhisper = str => console.log(str.toLowerCase());
+
+var sayHiToGrandma = str => {
+  if (str === str.toLowerCase()) return "I can't hear you!";
+  if (str === str.toUppercase()) return "YES INDEED!";
+  if (str === 'I love you, Grandma.') return "I love you, too."
+}
+
